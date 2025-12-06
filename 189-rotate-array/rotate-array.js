d1185433660dd9b51b4403c371648b618d5a2f60
Nagasaiprasad8019 <saiprasadsudhani@gmail.com>
2025-12-06 18:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 66.2 MB (47.69%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+var rotate = function(nums, k) {
+    let n=nums.length;
+    k=k%n;
+    reverse(nums,0,n-1);
+    reverse(nums,0,k-1);
+    reverse(nums,k,n-1);
+};
+var reverse = (nums,i,j)=>{
+    while(i<j){
+        let temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+        i++;
+        j--;
+    }
+}
