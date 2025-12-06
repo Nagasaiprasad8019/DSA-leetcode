1e3c6caa6774274c6f05ff79c8a9165674b8d09d
Nagasaiprasad8019 <saiprasadsudhani@gmail.com>
2025-12-06 18:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 60.6 MB (38.46%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -6,7 +6,7 @@ var findMaxConsecutiveOnes = function(nums) {
     let max=0;
     let count=0;
     for(let i=0;i<nums.length;i++){
-        if(nums[i]==0){
+        if(nums[i]===0){
             count=0;
         }
         else{
